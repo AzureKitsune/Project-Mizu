d89087defb1e3e8a5a02d468548b77deabb0e6cb
Amrykid <amrykid@gmail.com>
2011-08-13 16:02:02 -0500

    Got embedded loops to work. YAY!

--- Mizu/LocalBuilderEx.cs
+++ Mizu/LocalBuilderEx.cs
@@ -19,5 +19,15 @@ namespace Mizu
     {
         public LocalBuilder Base { get; set; }
         public string Name { get; set; }
+        public LocalType Type { get; set; }
+        public Label LoopLabel { get; set; }
+        public int LoopLow { get; set; }
+        public int LoopHigh { get; set; }
+        public Action LoopAction { get; set; }
+    }
+    public enum LocalType
+    {
+        Var = 0,
+        LoopVar = 1,
     }
 }
--- Mizu/Program.cs
+++ Mizu/Program.cs
@@ -13,6 +13,7 @@ namespace Mizu
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Mizu Compiler v" + Assembly.GetExecutingAssembly().GetName().Version.ToString());
             if (args.Length >= 2)
             {
@@ -67,7 +68,7 @@ namespace Mizu
             TypeBuilder tb = mb.DefineType("App");
             MethodBuilder entrypoint = tb.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static);
 
-            var ilGen = entrypoint.GetILGenerator(); //gets the IL generator
+            var ILgen = entrypoint.GetILGenerator(1024); //gets the IL generator
 
 
             List<LocalBuilderEx> locals = new List<LocalBuilderEx>(); //A list to hold variables.
@@ -77,14 +78,38 @@ namespace Mizu
             {
                 //Iterate though the statements
                 var basestmt = statement.Nodes[0];
-                HandleStatement(basestmt, ilGen, ref locals, out err);
+                HandleStatement(basestmt, ILgen, ref locals, out err);
                 if (err == true)
                 {
                     return;
                 }
             }
+            var loops = locals.FindAll(it => it.Type == LocalType.LoopVar);
+            if (loops.Count > 0)
+            {
+                int i = loops.Count -1;
+                while(i != -1)
+                {
+                    var lastloop = loops[i];
+                    if (lastloop != null)
+                    {
 
-            ilGen.Emit(OpCodes.Ret); //Finishes the statement by calling return
+                        lastloop.LoopAction(); //Calls actions to be called after all of the statements.
+
+                        //Checks if the current iterator is equal to the higher number.
+                        ILgen.Emit(OpCodes.Ldloc, lastloop.Base.LocalIndex);
+                        ILgen.Emit(OpCodes.Ldc_I4, lastloop.LoopHigh);
+                        ILgen.Emit(OpCodes.Clt);
+                        ILgen.Emit(OpCodes.Brtrue, lastloop.LoopLabel);
+
+                    }
+                    i -= 1;
+                }
+            }
+
+
+
+            ILgen.Emit(OpCodes.Ret); //Finishes the statement by calling return
 
             ab.SetEntryPoint(entrypoint); //Sets entry point
 
@@ -106,46 +131,76 @@ namespace Mizu
 
                             if (token.Token.Type == TokenType.IDENTIFIER) //If its a var declaration.
                             {
-                                var set = stmt.Nodes[i + 1];
-                                if (set.Token.Type == TokenType.SET)
+                                if (locals.Find(it => it.Name == token.Token.Text) == null)
                                 {
-                                    i += 1;
-                                    var next = stmt.Nodes[i + 1];
-                                    if (next.Token.Type == TokenType.NUMBER)
+                                    var set = stmt.Nodes[i + 1];
+                                    if (set.Token.Type == TokenType.SET)
                                     {
-                                        //Declares a variable and leaves a reference to it.
-
-                                        if (locals.Find(it => it.Name == token.Token.Text) == null)
+                                        i += 1;
+                                        var next = stmt.Nodes[i + 1];
+                                        if (next.Token.Type == TokenType.NUMBER)
                                         {
+                                            //Declares a variable and leaves a reference to it.
 
-                                            ILgen.DefineLabel();
                                             LocalBuilderEx local = new LocalBuilderEx();
                                             local.Base = ILgen.DeclareLocal(typeof(int));
                                             ILgen.Emit(OpCodes.Ldc_I4, int.Parse(next.Token.Text)); //Sets the number
                                             ILgen.Emit(OpCodes.Stloc, (LocalBuilder)local.Base); //Assigns the number to the variable.
 
                                             local.Name = token.Token.Text;
+                                            local.Type = LocalType.Var;
 
-                                            locals.Add(local); //Remmebers the variable.
+                                            locals.Add(local); //Remembers the variable.
                                             i += 1;
                                         }
                                         else
                                         {
-                                            //Report an error and stop compile process.
-                                            err = true;
-                                            Console.Error.WriteLine("'{0}' already exist!", token.Token.Text);
-                                            break;
+                                            //Its a range
+                                            var lowNum = stmt.Nodes[i + 2];
+                                            var highNum = stmt.Nodes[i + 5];
+
+                                           
+
+                                            LocalBuilderEx local = new LocalBuilderEx();
+
+                                            local.LoopHigh = int.Parse(highNum.Token.Text);
+                                            local.LoopLow = int.Parse(lowNum.Token.Text);
+
+                                            var looplab = ILgen.DefineLabel();
+                                            local.Base = ILgen.DeclareLocal(typeof(int));
+                                            ILgen.Emit(OpCodes.Ldc_I4, local.LoopLow); //Sets the number
+                                            ILgen.Emit(OpCodes.Stloc, (LocalBuilder)local.Base); //Assigns the number to the variable.
+                                           
+                                            ILgen.MarkLabel(looplab);
+                                            //this is where the IL will execute.
+
+                                            local.LoopAction = () =>
+                                            {
+                                                //Updates the iterator by +1
+                                                ILgen.Emit(OpCodes.Ldloc, local.Base.LocalIndex);
+                                                ILgen.Emit(OpCodes.Ldc_I4_1);
+                                                ILgen.Emit(OpCodes.Add);
+                                                ILgen.Emit(OpCodes.Stloc, (LocalBuilder)local.Base);
+                                            };
+
+                                            
+
+                                            local.Name = token.Token.Text;
+                                            local.Type = LocalType.LoopVar;
+                                            local.LoopLabel = looplab;
+
+                                            locals.Add(local); //Remembers the variable.
+
+                                            i += 6;
                                         }
                                     }
-                                    else
-                                    {
-                                        //Its a range
-                                        var lowNum = stmt.Nodes[i + 2];
-                                        var highNum = stmt.Nodes[i + 5];
-
-                                        var local = ILgen.DeclareLocal(typeof(int[]));
-                                        i += 6;
-                                    }
+                                }
+                                else
+                                {
+                                    //Report an error and stop compile process.
+                                    err = true;
+                                    Console.Error.WriteLine("'{0}' already exist!", token.Token.Text);
+                                    break;
                                 }
                             }
                         }
@@ -153,6 +208,7 @@ namespace Mizu
                     }
                 case TokenType.PrintStatement:
                     {
+
                         ///Generates output by making a print statement.
                         var period = stmt.Nodes[0];
                         var outpt = stmt.Nodes[1];
