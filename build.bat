f1720c2476bafac22736ab18738f116fd58f920d
Amrykid <amrykid@gmail.com>
2011-08-23 11:37:43 -0500

    1.0.257.0 build.

--- build.bat
+++ build.bat
@@ -6,8 +6,10 @@ echo Compiling Mizu...
 REM Build the solution
 %windir%\Microsoft.NET\Framework\V4.0.30319\msbuild "Mizu.sln" /p:Configuration=Release
 mkdir bin
+mkdir bin\Scripts\
 copy Mizu\bin\Debug\Mizu.exe bin\Mizu.exe
 copy Mizu\bin\Debug\Mizu.Lib.Evaluator.dll bin\Mizu.Lib.Evaluator.dll
 copy Mizu\bin\Debug\Mizu.Parser.dll bin\Mizu.Parser.dll
+copy Mizu\Scripts\ bin\Scripts\
 echo Done
 start bin
