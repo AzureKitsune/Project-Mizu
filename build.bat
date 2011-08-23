0fffccb45673e3948a23e48cbb0d07a6e872ccb6
Amrykid <amrykid@gmail.com>
2011-08-23 11:47:45 -0500

    Fail on my part. Build.bat copies Debug binaries instead of Release ones. This is now fixed.

--- build.bat
+++ build.bat
@@ -7,9 +7,9 @@ REM Build the solution
 %windir%\Microsoft.NET\Framework\V4.0.30319\msbuild "Mizu.sln" /p:Configuration=Release
 mkdir bin
 mkdir bin\Scripts\
-copy Mizu\bin\Debug\Mizu.exe bin\Mizu.exe
-copy Mizu\bin\Debug\Mizu.Lib.Evaluator.dll bin\Mizu.Lib.Evaluator.dll
-copy Mizu\bin\Debug\Mizu.Parser.dll bin\Mizu.Parser.dll
+copy Mizu\bin\Release\Mizu.exe bin\Mizu.exe
+copy Mizu\bin\Release\Mizu.Lib.Evaluator.dll bin\Mizu.Lib.Evaluator.dll
+copy Mizu\bin\Release\Mizu.Parser.dll bin\Mizu.Parser.dll
 copy Mizu\Scripts\ bin\Scripts\
 echo Done
 start bin
