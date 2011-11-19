a360ca3fd195673d80bddce84a165dfe01f166ad
Amrykid <amrykid@gmail.com>
2011-11-19 15:42:19 -0600

    Initial commit for Mizu 3.

---
+++ Mizu3/Compiler/Compiler.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Compiler.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Mizu3.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Compiler
+    {
+    }
+}
